af56250765ccb1e87e67a44032f90db96a143364
Isaac  Tingo <newtone480@gmail.com>
2023-01-06 16:32:46 +0300

    Settings

--- src/navigators/settings-nav.js
+++ src/navigators/settings-nav.js
@@ -2,16 +2,26 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Settings from "../screens/general/dashboard/settings";
-import Profile from "../screens/general/dashboard/profile";
-import EditProfile from "../screens/general/dashboard/edit-profile";
+import colors from "../componets/colors/colors";
+
+import EditProfile from "../screens/general/profile/edit-profile";
+import Profile from "../screens/general/profile/profile";
+import Settings from "../screens/general/profile/settings";
 
 const Stack = createNativeStackNavigator();
 
 export default function SettingsNav() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Settings" component={Settings} />
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: colors.lightBlue,
+        headerTitleAlign: "center",
+        headerStyle: {
+          backgroundColor: colors.bar,
+        },
+      }}
+    >
+      <Stack.Screen options={{}} name="Settings" component={Settings} />
       <Stack.Screen name="Profile" component={Profile} />
       <Stack.Screen name="EditProfile" component={EditProfile} />
     </Stack.Navigator>
---
+++ src/screens/general/profile/settings.js
@@ -0,0 +1,7 @@
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import styles from "../../../componets/styles/global-styles";
+
+export default function Settings() {
+  return <ScrollView style={styles.container}></ScrollView>;
+}
