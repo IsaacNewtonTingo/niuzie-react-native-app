793993a67c29c795c36072e3aba8b916a699bacf
Isaac Newton Tingo <newtone480@gmail.com>
2023-01-29 14:29:50 +0300

    Get saved products

--- src/screens/general/profile/saved-products.js
+++ src/screens/general/profile/saved-products.js
@@ -1,12 +1,84 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import styles from "../../../componets/styles/global-styles";
+import { ScrollView, StyleSheet, Text, FlatList, View } from "react-native";
+import React, { useEffect, useState, useContext } from "react";
 
-export default function SavedProducts() {
+import styles from "../../../componets/styles/global-styles";
+import HorizontalCard from "../../../componets/cards/horizontal-card";
+
+import { CredentialsContext } from "../../../componets/context/credentials-context";
+import LoadingIndicator from "../../../componets/preloader/loadingIndicator";
+import axios from "axios";
+
+export default function SavedProducts({ navigation }) {
+  const { storedCredentials, setStoredCredentials } =
+    useContext(CredentialsContext);
+
+  const { data } = storedCredentials;
+
+  const userID = data.userID;
+  const token = data.token;
+
+  const [loading, setLoading] = useState(true);
+  const [loadingData, setLoadingData] = useState(true);
+
+  const [savedItems, setSavedItems] = useState([]);
+
+  useEffect(() => {
+    getSavedItems();
+  }, [(navigation, loading)]);
+
+  navigation.addListener("focus", () => setLoading(!loading));
+
+  async function getSavedItems() {
+    const url = `${process.env.ENDPOINT}/product/get-saved-products/${userID}`;
+
+    const headers = {
+      "auth-token": token,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+
+    await axios
+      .get(url, { headers })
+      .then((response) => {
+        setLoadingData(false);
+        console.log(response.data);
+        if (response.data.status == "Success") {
+          setSavedItems(response.data.data);
+        }
+      })
+      .catch((err) => {
+        setLoadingData(false);
+        console.log(err);
+      });
+  }
+
+  if (loadingData) {
+    return <LoadingIndicator />;
+  }
+
   return (
-    <ScrollView style={styles.container}>
-      <Text>SavedProducts</Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <FlatList
+        data={savedItems}
+        renderItem={({ item }) => (
+          <HorizontalCard
+            key={item._id}
+            productImage1={item.product.image1}
+            productImage2={item.product.image2}
+            productImage3={item.product.image3}
+            productImage4={item.product.image4}
+            productName={item.product.productName}
+            price={item.product.price}
+            condition={item.product.condition}
+            description={item.product.description}
+            county={item.product.user.county}
+            subCounty={item.product.user.subCounty}
+            rating={item.product.rating.$numberDecimal}
+            premium={item.product.user.premium}
+          />
+        )}
+      />
+    </View>
   );
 }
 
