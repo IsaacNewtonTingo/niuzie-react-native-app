b4deb747b280d959c558c125f6feda285587c35a
Isaac  Tingo <newtone480@gmail.com>
2023-01-31 17:25:13 +0300

    Nav

--- src/screens/admin/dashboard/admin-dashboard.js
+++ src/screens/admin/dashboard/admin-dashboard.js
@@ -4,11 +4,6 @@ import React, { useState, useEffect, useContext } from "react";
 import { CredentialsContext } from "../../../componets/context/credentials-context";
 
 export default function AdminDashboard() {
-  const { storeCredentials, setStoreCredentials } =
-    useContext(CredentialsContext);
-
-  const { data } = storeCredentials;
-  const userID = data.userID;
   return (
     <ScrollView>
       <Text>Home</Text>
