d12344b78afccc4974d10ecb4881cdbff0580dac
Isaac Newton Tingo <newtone480@gmail.com>
2023-02-03 09:08:31 +0300

    Notifications

--- src/screens/general/dashboard/notifications.js
+++ src/screens/general/dashboard/notifications.js
@@ -1,8 +1,18 @@
 import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React,{useEffect,useState,useContext} from "react";
 import styles from "../../../componets/styles/global-styles";
+import { CredentialsContext } from "../../../componets/context/credentials-context";
+
 
 export default function Notifications() {
+    const { storedCredentials, setStoredCredentials } =
+    useContext(CredentialsContext);
+  
+
+  
+  async function getNotifications() {
+    
+  }
   return <ScrollView style={styles.container}></ScrollView>;
 }
 
